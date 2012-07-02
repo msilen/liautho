91c27f8bb52c3744ca35f8ef9d3a19faae158687
Dark <dark@soulspark.(none)>
2012-07-02 18:58:26 +0900

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liautho::Application.config.secret_token = 'b594873640e9ba9cb55e85ba4d3a44db618d6ff0880a827f81b47dcd64abb76da054db6461661ec2951aabe8a49e6f992f86d0b5f6d4dd46b9782b1892bce93c'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Liautho::Application.routes.draw do
+  root :to => "home#index"
+end
